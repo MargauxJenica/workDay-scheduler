67bedc62f2f1361128ede2e895d6f6c0f28cbe0b
Margaux <margaux.santos113@gmail.com>
2024-01-31 00:34:19 -0800

    saveBtn event handler and local storage

---
+++ Develop/script.js
@@ -0,0 +1,48 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+// $(document).ready(function(){} (vanilla JS of the function below)
+$(function () {
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+
+
+  // event handler to save user's input
+  // grabbing the element with the class of saveBtn 
+  $(".saveBtn").on("click", function () {
+
+    console.log ('saveBtn was clicked');
+    
+    // `this` for both blockHour and eventText refers to saveBtn
+    // getting the immediate parent of the element with the `saveBtn` class
+    // and grabbing the value of id at the second part [1] of "hour-x"
+    var blockHour = $(this).parent().attr("id").split("-")[1];
+
+    // gets all siblings element with the class "description"
+    // and grabbs the text within those siblings
+    var eventText = $(this).siblings(".description").val();
+
+    // sets an item in the local storage with 
+    // "event-hour" ("event-9") and the value of the text
+    localStorage.setItem("event-" + blockHour, eventText);
+  });
+
+
+});
